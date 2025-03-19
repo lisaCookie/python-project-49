a108d4b6dff2fe012b53c3dd2656ab7bc0d1b45e
lisa <lisavetabelova@gmail.com>
2025-03-19 21:53:03 +0300

    modified brain_games/cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,6 @@
+import prompt
+
 def welcome_user():
-    name = ''
-    while name == '':
-        print('May I have your name? ', end='')
-        name = input()
+    name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     return name
-
